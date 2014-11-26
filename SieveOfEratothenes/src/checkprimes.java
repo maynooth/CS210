37afdfb9376f88f744d2dacdf49f61d40216c91d
danielpaul <danielpaul2008@gmail.com>
2014-11-26 15:51:43 +0000

    imporved check prime code

--- SieveOfEratothenes/src/checkprimes.java
+++ SieveOfEratothenes/src/checkprimes.java
@@ -1,47 +1,57 @@
 import java.util.Scanner;
 
 public class checkprimes {
-	public static void main(String args[]) {
 
-		Scanner scan = new Scanner(System.in);
-		System.out.println("Please enter prime to find");
-		int num = scan.nextInt();
-		int length = (int) (2 * num * Math.log(num));
+  public static void main(String args[]) {
 
-		boolean[] myArray = new boolean[length];
-		long start = System.currentTimeMillis();
 
-		for (int i = 2; i < myArray.length; i++) {
-			myArray[i] = true;
-		}
+    Scanner scan = new Scanner(System.in);
 
-		for (int i = 2; i < myArray.length; i++) {
-			for (int j = 2;; j++) {
-				int mul = i * j;
-				if (mul > myArray.length) {
-					break;
-				}
-				if (mul < myArray.length) {
-					myArray[mul] = false;
-				}
+    // ask user for input
+    System.out.print("Please enter prime to find: ");
+    int find = scan.nextInt();
 
-			}
-		}
-		int counter = 0;
-		for (int k = 2; k < myArray.length; k++) {
+    int array_size = (int) (2 * find * Math.log(find));
 
-			if (myArray[k] == true) {
-				counter++;
-				if (num == counter) {
-					System.out.println(k);
-					long elapsed = System.currentTimeMillis() - start;
-					System.out.println("The time taken is " + elapsed);
-					break;
+    boolean[] myArray = new boolean[array_size];
 
-				}
-			}
+    // ture and false the other way around from lab notes
 
-		}
+    long start = System.currentTimeMillis();
 
-	}
+    // loop through numbers from 2 to size of array
+    for(int i = 2; i < myArray.length; i++) {
+
+      if(myArray[i] == true) continue; // is not a prime number
+
+      // System.out.println(i); // print out the number
+
+      // set all multiples of this number to true
+      int mul = i;
+      for(int times = 2; mul < myArray.length; times++) {
+        mul = i * times;
+        if(mul < myArray.length) myArray[mul] = true; // set to true if that number is in array
+      }
+
+    }
+
+    // print out the nth prime number 
+    int nth = 0;
+    for(int i = 2; nth <= find; i++) {
+      if(myArray[i] == false) { // is prime number
+        nth++;
+        if(nth == find) {
+          System.out.println("The " + find + "th prime number is: " + i);
+          break;
+        }
+      }
+    }
+
+
+    long elapsed = System.currentTimeMillis() - start;
+    System.out.println("The answer took " + elapsed + " milliseconds to compute");
+
+
+  }
+
 }
