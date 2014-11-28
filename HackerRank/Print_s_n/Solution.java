34d53fc164034f64ff8009a677af71fd18d21b49
danielpaul <danielpaul2008@gmail.com>
2014-11-28 13:57:55 +0000

    shortened code for hackerrank print s n solution

--- HackerRank/Print_s_n/Solution.java
+++ HackerRank/Print_s_n/Solution.java
@@ -7,17 +7,19 @@ import java.util.regex.*;
 public class Solution {
 
 	public static void main(String[] args) {
+		
 		try {
-			int n = 0;
-			String s = new String("");
+		
 			BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-			n = Integer.parseInt(stdin.readLine());
-			s = stdin.readLine();
-			for (int i = 1; i <= n; i++) {
-				System.out.println(s);
-			}
+			int n = Integer.parseInt(stdin.readLine());
+			String s = stdin.readLine();
+			
+			for (int i = 1; i <= n; i++) System.out.println(s);
+		
 		} catch (IOException io) {
 			io.printStackTrace();
 		}
+
 	}
+
 }
